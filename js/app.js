6952737febdc8c0b1f44fbaa4c302de654476330
Temak <makindeoluwatimilehin@gmail.com>
2020-07-08 19:59:05 +0100

    responsive updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-$('.item').click(function() {
-  $(this).toggleClass('active');
+$(document).ready(function(){
+  
+  //Services Mobile Sliders
+  $('.sliders').slick({
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    dots: true,
+    prevArrow: $('.prev-btn'),
+    nextArrow: $('.next-btn'),
+  })
 });
